3885429ffc77082f30282d41c33577a0689e4794
hdh4202 <aaabbb4202@naver.com>
2025-01-20 17:45:30 +0900

    추천 여행 도시 이름 한글화
    추천 여행 도시 이름 한글화
    app2.py에 app_hwang 코드 업데이트

--- app_hwang.py
+++ app_hwang.py
@@ -15,7 +15,14 @@ def recommand_hwang():
     "Los Angeles", "Amsterdam", "Hong Kong", "Berlin", "Sydney",
     "Venice", "Prague", "Seoul", "San Francisco", "Kyoto"
     ]
+    citys_kor = [
+    "파리", "뉴욕", "도쿄", "런던", "로마",
+    "두바이", "바르셀로나", "이스탄불", "방콕", "싱가포르",
+    "로스앤젤레스", "암스테르담", "홍콩", "베를린", "시드니",
+    "베네치아", "프라하", "서울", "샌프란시스코", "교토"
+    ]
     weather_data = []
+    cnt = 0
     for city in citys:         
         apikey = 'f14638e80f750690b27c459de0db1173'                             # 해당 지역
         # apikey = os.getenv("API_KEY")    # API 키
@@ -28,16 +35,17 @@ def recommand_hwang():
         data = json.loads(result.text) 
         link_city = f"recommand_{city}.html"
         
-        c_time = datetime.now().strftime("%Y-%m-%d")
-        
+        c_time = datetime.now().strftime("%Y-%m-%d")        
+
         # 필요한 데이터 추출     
         w_data = {
-            "city": city,
+            "city": citys_kor[cnt],
             "temp": data["main"]["feels_like"],
             "weather": data["weather"][0]["description"], 
             "detail": link_city,
             "update_time": c_time
         }
+        cnt += 1
         if w_data["temp"] > 12 and w_data["temp"] < 32:
             weather_data.append(w_data)    
         
